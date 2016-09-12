32fc0a27cf5877db1585d685ffb8d81b89ab3ef4
Michael Appleby <michael.appleby@me.com>
2016-09-11 23:31:09 -0400

    Add postmessage

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,9 +1,18 @@
+require 'json'
 class TokenController < ApplicationController
 
   def supply_token
     json = Hash.new
     json['accessToken'] = session[:iiif_auth]
-    render json: json
+    json['messageId'] = params[:messageId] if params[:messageId]
+    @domain = params[:domain]
+    @values = JSON.unparse(json).html_safe
+    status = 500 if @domain.nil?
+    respond_to do |format|
+      format.json { render json: json }
+      format.html { render 'index' }
+    end
+
   end
 
 end
